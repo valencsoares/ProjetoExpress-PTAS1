e2e085d0b25cc7cf27f06aa1c84ff2d7137574e6
valencsoares <pvcampossoares558@gmail.com>
2023-09-12 14:29:29 -0400

    anotações e criação da rota e pasta contatos

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//a próxima linha é um controller, o "/" é uma rota (a ideia do controle e a rota é a mesma)
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Olá, Mundo!' }); //render: torna visual, como ele joga para o usuário, usa-se a resposta
+});
+
+app.get('/contatos', (req, res) => {
+  res.render('contatos', { message: 'Página de contatos!' });
 });
 
 app.listen(port, () => {
